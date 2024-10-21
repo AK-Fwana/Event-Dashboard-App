ff411c06efeb85c784f16014b7ed6c1f1a26fdae
AK-Fwana <111739586+AK-Fwana@users.noreply.github.com>
2024-10-21 18:21:36 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,27 +6,47 @@ import { EventsPage } from "./pages/EventsPage";
 import { EventPage } from "./pages/EventPage";
 import { Root } from "./components/Root";
 
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Root />,
+//     children: [
+//       {
+//         path: "/",
+//         element: <EventsPage />, 
+//       },
+//       {
+//         path: "/event/:eventId",
+//         element: <EventPage />,
+//       },
+//     ],
+//   },
+// ]);
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <ChakraProvider>
+//       <RouterProvider router={router} /> 
+//     </ChakraProvider>
+//   </React.StrictMode>
+// );
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Root />,
     children: [
-      {
-        path: "/",
-        element: <EventsPage />, // Route for listing events
-      },
-      {
-        path: "/event/:eventId",
-        element: <EventPage />, // Dynamic route for event details
-      },
+      { path: '/', element: <EventsPage /> },
+      { path: '/event/:eventId', element: <EventPage /> },
     ],
   },
-]);
+], {
+  basename: "/Event-Dashboard-App",  // This matches the name of your GitHub repository
+});
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider>
-      <RouterProvider router={router} /> {/* React Router is used here */}
+      <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>
 );
